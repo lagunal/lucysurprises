4513c5e923b193d1644d9cb8b9ee25a55fbf9254
Ludwing Laguna <ludwinglaguna@gmail.com>
2019-06-10 22:24:35 -0400

    added description items home

--- src/App.js
+++ src/App.js
@@ -39,7 +39,7 @@ function App() {
     <section className="jumbotron text-center">
                 <div className="container">
                 {/* <h1 className="jumbotron-heading">Album example</h1> */}
-                <img src={require("./assets/img/LogoLucySurprises.png")} className="img-fluid"/>
+                <img src={'/assets/img/LogoLucySurprises.png'} className="img-fluid"/>
                 <p className="h1">"Because Every Day is a Special Day"</p>
                 {/* <p>
                     <a href="#" className="btn btn-primary my-2 mx-1">Main call to action</a>
@@ -136,35 +136,43 @@ function App() {
           english='It is a Balloon Bouquet in miniature version with Chic Style, seeks to recreate the motive to celebrate for our client in a simple way, with a touch of elegance and without excessive.'
           spanish='Es un Bouquet de Globos en versión miniatura con un estilo Chic, busca recrear el motivo a celebrar por nuestro cliente de una manera sencilla, con un toque de elegancia y sin excesos.'
           price='$99.99 - $115'
-          photo='../../assets/img/miniBouquet.jpeg'
+          photo='/assets/img/miniBouquet.jpeg'
       />
 
 
       <hr className="featurette-divider" />
 
-      <div className="row featurette">
-        <div className="col-md-7 order-md-2">
-          <h2 className="featurette-heading">Oh yeah, it’s that good. <span className="text-muted">See for yourself.</span></h2>
-          <p className="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
-        </div>
-        <div className="col-md-5 order-md-1">
-          <svg className="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: 500x500"><title>Placeholder</title><rect width="100%" height="100%" fill="#eee"/><text x="50%" y="50%" fill="#aaa" dy=".3em">500x500</text></svg>
-        </div>
-      </div>
+      <PhotoItem 
+          title='Small Bouquet '
+          miniDescription='Small and elegant...!'
+          english='it is a balloon bouquet with fresh style that stands out in plain sight and denotes with simplicity the motive to celebrate by our client.'
+          spanish='Es un Bouquet de Globos que destaca a la vista y denota con sencillez el motivo a celebrar por nuestro cliente.'
+          price='$120 - $134.99'
+          photo='/assets/img/smallBouquet.jpeg'
+      />
 
       <hr className="featurette-divider" />
 
-      <div className="row featurette">
-        <div className="col-md-7">
-          <h2 className="featurette-heading">And lastly, this one. <span className="text-muted">Checkmate.</span></h2>
-          <p className="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.</p>
-        </div>
-        <div className="col-md-5">
-          <svg className="bd-placeholder-img bd-placeholder-img-lg featurette-image img-fluid mx-auto" width="500" height="500" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: 500x500"><title>Placeholder</title><rect width="100%" height="100%" fill="#eee"/><text x="50%" y="50%" fill="#aaa" dy=".3em">500x500</text></svg>
-        </div>
-      </div>
+      <PhotoItem 
+          title='Medium Bouquet '
+          miniDescription='Small and elegant...!'
+          english='It is a Balloon Bouquet remarkable, which striking for its beautiful decorative details to celebrate and fest that person or moment super special for you. This is our Most Popular Bouquet, because cause a Impressive Surprise Feeling in the persons that receive them.'
+          spanish='Es un ramo de globos notable, que destaca por sus hermosos detalles decorativos para celebrar y festejar a esa persona o momento super especial para ti. Este es nuestro ramo más popular, porque causa una sensación de sorpresa impresionante en las personas que los reciben.'
+          price='$140 - $169.99'
+          photo='/assets/img/mediumBouquet.jpeg'
+      />
 
+   
       <hr className="featurette-divider" />
+
+      <PhotoItem 
+          title='Large Bouquet '
+          miniDescription='Small and elegant...!'
+          english='It is a balloon bouquet with fresh style que destaca a la vista y denota con sencillez el motivo a celebrar por nuestro cliente.'
+          spanish=''
+          price='$180 - $209.99'
+          photo='/assets/img/largeBouquet.jpeg'
+      />
 
     </div>
 
