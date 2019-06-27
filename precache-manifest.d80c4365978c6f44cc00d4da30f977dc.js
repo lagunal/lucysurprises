12a01e54cd2742809a509dbdb502d329f97034cc
Ludwing Laguna <ludwinglaguna@gmail.com>
2019-06-26 21:58:43 -0400

    Updates

---
+++ precache-manifest.d80c4365978c6f44cc00d4da30f977dc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c852ef03e9edcd933fb0c84caca59acc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5e0303500df701445000",
+    "url": "/static/css/main.cff5f1a4.chunk.css"
+  },
+  {
+    "revision": "2315f74ce3c99316133d",
+    "url": "/static/js/2.3310f33a.chunk.js"
+  },
+  {
+    "revision": "5e0303500df701445000",
+    "url": "/static/js/main.6ceac567.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
