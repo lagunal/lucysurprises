ab953eb8d9ed5fbe53badcbb26e537eb6ee1242b
Ludwing Laguna <ludwinglaguna@gmail.com>
2019-08-24 01:43:48 -0400

    Updates

---
+++ precache-manifest.f499363d65030a4ab01954ea261c11b6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e99a6ee6f06912ae6204c3e5a79df4c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "482d2e4e3b5c46627a6e",
+    "url": "/static/css/main.cff5f1a4.chunk.css"
+  },
+  {
+    "revision": "56509b8fd56798a6bf7c",
+    "url": "/static/js/2.2c73a79e.chunk.js"
+  },
+  {
+    "revision": "482d2e4e3b5c46627a6e",
+    "url": "/static/js/main.45fa7c97.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "ddaa61787534ec39af97192ec544e7eb",
+    "url": "/static/media/PriceGuide1.ddaa6178.png"
+  },
+  {
+    "revision": "8f6fedc183f18842875f173cc21148ee",
+    "url": "/static/media/PriceGuide2_.8f6fedc1.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8244e0454975544bbb3e624084f3fed6.js"
+  "/precache-manifest.f499363d65030a4ab01954ea261c11b6.js"
 );
 
 self.addEventListener('message', (event) => {
